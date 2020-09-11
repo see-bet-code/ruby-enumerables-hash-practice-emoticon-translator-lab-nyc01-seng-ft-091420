4310a8b97d1ca91af389bcb64e9857db3e518f83
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-09-11 15:25:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,24 +3,25 @@ require "yaml"
 
 def load_library(file_path)
   # code goes here
-  emoticons = {}
+  hash = {}
   lib = YAML.load_file(file_path)
   lib.each { | name, versions |
-    emoticons[name] = {
+    hash[name] = {
       :english => versions[0],
       :japanese => versions[1]
     }
   }
-  emoticons
+  hash
 end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
-  lib = YAML.load_file(file_path)
-
+  emoticons = load_library(file_path)
+  emoticons[emoticon][:japanese]
 end
 
 def get_english_meaning(file_path, emoticon)
   # code goes here
-  lib = YAML.load_file(file_path)
+  emoticons = load_library(file_path)
+  emoticons[emoticon][:english]
 end
