5d20876cc4a258587d13b4f07618352897d1d0d4
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-09-11 15:23:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,9 +16,9 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
-  
+
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   # code goes here
 end
