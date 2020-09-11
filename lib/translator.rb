c320597ebda15032d4000f30d0bdc8662e73d529
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-09-11 15:39:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,6 +23,6 @@ def get_english_meaning(file_path, input)
   # code goes here
   lib = load_library(file_path)
   lib.each { |meaning, emoticons|
-    return meaning if emoticons[meaning][emoticons[1]] == input
+    return meaning if emoticons[meaning][:japanese] == input
   }
 end
