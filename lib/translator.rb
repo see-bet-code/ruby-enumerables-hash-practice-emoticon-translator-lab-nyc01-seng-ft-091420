11a26994f4fbb47e4f093f9b8375e989f939734d
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-09-11 15:26:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,11 +17,10 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   emoticons = load_library(file_path)
-  emoticons[emoticon][:japanese]
 end
 
 def get_english_meaning(file_path, emoticon)
   # code goes here
   emoticons = load_library(file_path)
-  emoticons[emoticon][:english]
+  
 end
